b514f35ae0520aa8f69b70612862dd3c8c1214d7
Mathieu Federak <mfederak@gmail.com>
2018-02-13 15:30:13 +0100

    CRUD finished, no CSS

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,16 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # CRUD
-  # Read
-  # Read all
-  get 'tasks', to: 'tasks#index'
 
-  # Read only one
-  get 'tasks/:id', to: 'tasks#show'
-
-  # Create
-  get 'tasks/new', to: 'tasks#new'
+  get 'tasks', to: 'tasks#index', as: :tasks
   post 'tasks', to: 'tasks#create'
-
-  # Update
-  get 'tasks/:id/edit', to: 'tasks#edit'
-  patch 'tasks', to: 'tasks#update'
-
-  # Destroy
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  patch 'tasks/:id', to: 'tasks#update'
   delete 'tasks/:id', to: 'tasks#destroy'
+
+
+# resources :tasks
 
 end
